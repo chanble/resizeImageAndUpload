7188daa4661e269ba2ffd165e3e6829b8e41f8b3
chenbo <chenbo@bxd365.com>
2016-08-19 14:51:09 +0800

    完成了formdata上传文件

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-
 var_dump($_FILES);
+if (!empty($_FILES)) {
+    foreach ($_FILES as $key => $file) {
+        move_uploaded_file($file['tmp_name'], 'upload' . DIRECTORY_SEPARATOR . $file['name']);
+    }
+}
